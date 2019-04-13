f73d77c32f611eedf91c5881ede607ba61e7bb9d
Markus Oberlehner <markus.oberlehner@gmail.com>
2019-04-13 15:45:06 +0200

    Handle multiple requests on same component

--- src/utils/external-component.js
+++ src/utils/external-component.js
@@ -1,6 +1,9 @@
 export default function externalComponent(url) {
-  return new Promise((resolve, reject) => {
-    const name = url.split(`/`).reverse()[0].match(/^(.*?)\.umd/)[1];
+  const name = url.split(`/`).reverse()[0].match(/^(.*?)\.umd/)[1];
+
+  if (window[name]) return window[name];
+
+  window[name] = new Promise((resolve, reject) => {
     const script = document.createElement(`script`);
     script.async = true;
     script.addEventListener(`load`, () => {
@@ -12,4 +15,6 @@ export default function externalComponent(url) {
     script.src = url;
     document.head.appendChild(script);
   });
+
+  return window[name];
 }
